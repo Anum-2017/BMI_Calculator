253c45ee788ed67057761b3b380b8ecf7bacc5ed
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-03-28 14:13:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//BMI CALCULATOR
+let weight = 70;
+let height = 1.78;
+let bmi = weight / (height * height);
+console.log(`Your BMI is ${bmi}`);
---
+++ main.ts
@@ -0,0 +1,9 @@
+//BMI CALCULATOR
+
+let weight = 70;
+
+let height = 1.78;
+
+let bmi = weight / (height * height)
+
+console.log(`Your BMI is ${bmi}`);
